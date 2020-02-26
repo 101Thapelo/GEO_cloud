72a3a87a45afba8c53a80129b5a40fb665598244
101Thapelo <50619622+101Thapelo@users.noreply.github.com>
2020-02-26 11:40:15 +0200

    Update inspection.page.ts
    Co-Authored-By: lezoe <lezoe@users.noreply.github.com>
    Co-Authored-By: Isaac  <isaacdev57@users.noreply.github.com>
    Co-Authored-By: Kamogelo-S-Kgari <kamogelo-s-kgari@users.noreply.github.com>
    Co-Authored-By: shanabmv83 <shanabmv83@users.noreply.github.com>
    Co-Authored-By: Mothomogolo <mothomogolo@users.noreply.github.com>

--- src/app/inspection/inspection.page.ts
+++ src/app/inspection/inspection.page.ts
@@ -7,10 +7,29 @@ import { NavController } from '@ionic/angular/providers/nav-controller';
   styleUrls: ['./inspection.page.scss'],
 })
 export class InspectionPage implements OnInit {
+  [x: string]: any;
   currentImage: any
-  constructor(public navCtrl: NavController, private camera: Camera) { }
- 
+  constructor(public navCtrl: NavController, private camera: Camera) { 
+
+    
+  }
+ takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.Camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue:" + err);
+    });
+  }
   ngOnInit() {
+    
     
     }
   }
